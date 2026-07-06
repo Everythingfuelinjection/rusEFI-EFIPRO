62b76f0c18a52b60df8374c3ec2600fbb6a6521f
GitHub git update Action <action@github.com>
2026-07-06 02:55:25 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_rusEFI_EFIPRO.h
+++ generated/controllers/generated/signature_rusEFI_EFIPRO.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1425562284
-#define TS_SIGNATURE "rusEFI main.2026.07.05.rusEFI_EFIPRO.1425562284"
+#define SIGNATURE_HASH 3210245229
+#define TS_SIGNATURE "rusEFI main.2026.07.06.rusEFI_EFIPRO.3210245229"
