e90b61b4ca1f6eb1d57784b651eb107d0c5946be
GitHub git update Action <action@github.com>
2026-07-12 02:31:18 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_rusEFI_EFIPRO.h
+++ generated/controllers/generated/rusefi_generated_rusEFI_EFIPRO.h
@@ -2237,7 +2237,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.07.11.rusEFI_EFIPRO.2178177585"
+#define TS_SIGNATURE "rusEFI main.2026.07.12.rusEFI_EFIPRO.2178177585"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
